5f9a37514b2a60df076d15091406c408fc530b79
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2017-04-19 23:31:48 +0700

    temporary update to initialize SSLContext with TLSv1

--- lib/houston/connection.rb
+++ lib/houston/connection.rb
@@ -35,7 +35,7 @@ module Houston
 
       @socket = TCPSocket.new(@uri.host, @uri.port)
 
-      context = OpenSSL::SSL::SSLContext.new
+      context = OpenSSL::SSL::SSLContext.new(:TLSv1)
       context.key = OpenSSL::PKey::RSA.new(@certificate, @passphrase)
       context.cert = OpenSSL::X509::Certificate.new(@certificate)
 
